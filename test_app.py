49519994161945c31b6edaaa887d135b9cd60cd4
Deepthi-p-rao <deepthi.hprao98@email.com>
2026-05-15 19:57:40 -0700

    Added health check

--- test_app.py
+++ test_app.py
@@ -2,7 +2,7 @@ from app import app
 
 def test_home():
     client = app.test_client()
-    response = client.get('/')
+    response = client.get('/health')
 
     assert response._status_code == 200
     assert b"CICD pipeline working!" in response.data
